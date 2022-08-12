5213d89941fb6d30aad2253eff1907b6e64beade
Zachary Piontek <fortunefavorsthebrave@protonmail.com>
2022-08-12 10:02:01 -0700

    redirect test passing fixed redirect pathway

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,8 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+
+jest.mock('../lib/services/github');
   
 describe('backend-express-template routes', () => {
   beforeEach(() => {
@@ -10,18 +12,28 @@ describe('backend-express-template routes', () => {
   
   it('should redirect to the github oauth page upon login', async () => {
     const res = await request(app).get('/api/v1/github/login');
-    console.log(res.header.location);
+    // console.log(res.header.location);
     expect(res.header.location).toMatch(
       'https://github.com/login/oauth/authorize?client_id=7524060cc5a16f129bc4&scope=user&redirect_uri=http://localhost:7890/api/v1/github/callback'
     );
   });
+
+  // // stretch test :)
+  // it('should redirect to the github oauth page upon login', async () => {
+  //   const res = await request(app).get('/api/v1/github/login');
+  
+  //   expect(res.header.location).toMatch(
+  //     /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/callback/i
+  //   );
+  // });
       
   it('should login and redirect users to /api/v1/github/dashboard', async () => {
     const res = await request
       .agent(app)
-      .get('/api/v1/github/login/callback?code=42')
+      .get('/api/v1/github/callback?code=42')
       .redirects(1);
-        
+      
+    // console.log(res);
     console.log(res.body);
     expect(res.body).toEqual({
       id: expect.any(String),
