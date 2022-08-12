9ceef4c9ce82a46f41efd868da740843418ef6db
Zachary Piontek <fortunefavorsthebrave@protonmail.com>
2022-08-12 10:47:06 -0700

    tables setup for posts

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,7 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS github_user;
+DROP TABLE IF EXISTS gitty_post;
 
 CREATE TABLE github_user (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -8,3 +9,12 @@ CREATE TABLE github_user (
   username TEXT NOT NULL,
   avatar TEXT
 );
+
+CREATE TABLE gitty_post (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  post VARCHAR(255)
+);
+
+INSERT INTO gitty_post (post)
+VALUES
+('DRINK COFFEE: do stupid things faster with more energy');
